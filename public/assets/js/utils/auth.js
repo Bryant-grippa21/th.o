66e6146020eb6ccc2cd0ce8c6a9762bb03dd8a44
Osu-JD66 <joshuazarache@gmail.com>
2026-04-07 12:59:43 -0400

    Delete public directory

--- public/assets/js/utils/auth.js
+++
@@ -1,8 +0,0 @@
-const getToken = () => localStorage.getItem('token');
-
-const isLogged = () => !!getToken();
-
-const logout = () => {
-  localStorage.removeItem('token');
-  window.location.href = '/auth/login.html';
-};
